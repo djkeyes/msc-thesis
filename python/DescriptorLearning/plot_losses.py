ce01eca6f8240614905be7434f2c4575902b2680
daniel keyes <dkeyes@student.ethz.ch>
2017-07-21 18:30:50 +0200

    Group losses into groups of iter_size=12

--- python/DescriptorLearning/plot_losses.py
+++ python/DescriptorLearning/plot_losses.py
@@ -8,16 +8,34 @@ import matplotlib.pyplot as plt
 # grep -e ";" train-descriptors-32.out | grep -oE "[^ ]+$" > losses-32.txt
 
 if __name__ == '__main__':
-  losses = np.loadtxt("/home/daniel/losses-32.txt")
+  losses = np.loadtxt("/home/daniel/losses-32-adam.txt")
   t = np.arange(0, len(losses))
 
-  # plt.scatter(t, losses, s=0.05)
-  # plt.title('Training Loss Over Time, 32D')
-  # plt.show()
+  plt.scatter(t, losses, s=0.05)
+  plt.title('Training Loss Over Time, 32D, ADAM')
+  plt.ylim((0, 0.12))
+  plt.show()
 
   series = pd.Series(losses, t)
   window_size=100
-  moving_med = series.rolling(window=window_size, center=False).median()
-  plt.plot(moving_med.index[window_size-1:], moving_med[window_size-1:])
-  plt.title('Training Loss Over Time, Rolling Median, 32D')
+  moving_avg = series.rolling(window=window_size, center=False).mean()
+
+  plt.plot(moving_avg.index[window_size - 1:], moving_avg[window_size - 1:])
+  plt.title('Training Loss Over Time, Rolling Median, 32D, ADAM')
+  plt.ylim((0, 0.12))
+  plt.show()
+
+  # In our tests, gradients are accumulated over iter_size=12 evaulations of the loss function
+  # Ergo group into groups of 12, then average. (Due to the extra printed lines during reshaping / other log output,
+  # this might be off by one or something. hopefully doesn't matter.)
+  # Also, the actual gradient is weighted by the number of point correspondences in each training sample. So for a more
+  # meaningful analysis, we ought to compute the number of valid point correspondences per training pair and reweight
+  #  by that.
+  iter_size=12
+  groups = series.groupby((series.index+iter_size-1)/iter_size)
+  group_means = groups.mean()
+
+  plt.plot(group_means)
+  plt.title('Training Loss Over Time, 32D, ADAM')
+  plt.ylim((0, 0.12))
   plt.show()
