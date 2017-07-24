9becd2ff20ec08c6d1f07b186315f83b440ad42c
daniel keyes <dkeyes@student.ethz.ch>
2017-07-24 16:53:49 +0200

    draw pretty blue GT/Estimated correspondences

--- python/plot_actual_vs_expected.py
+++ python/plot_actual_vs_expected.py
@@ -7,6 +7,10 @@ expected = np.loadtxt('../cpp/build/expected.txt')
 
 plt.plot(expected[:,0], expected[:,1], 'g-')
 plt.plot(actual[:,0], actual[:,1], 'r--')
+for i in range(len(actual)):
+  xs = [actual[i,0], expected[i,0]]
+  ys = [actual[i,1], expected[i,1]]
+  plt.plot(xs, ys, 'b-', lw=0.1)
 plt.show()
 
 
