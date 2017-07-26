f1dfd5e820f5237f98fe07da911643cd323fdfbb
daniel keyes <dkeyes@student.ethz.ch>
2017-07-26 20:01:29 +0200

    also show 3d trajectories
    honestly they look okay. how's the rotation? Alternatively, try proper sim3 alignment.

--- python/plot_actual_vs_expected.py
+++ python/plot_actual_vs_expected.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 actual = np.loadtxt('../cpp/build/actual.txt')
 expected = np.loadtxt('../cpp/build/expected.txt')
@@ -11,6 +12,17 @@ for i in range(len(actual)):
   xs = [actual[i,0], expected[i,0]]
   ys = [actual[i,1], expected[i,1]]
   plt.plot(xs, ys, 'b-', lw=0.1)
+plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(expected[:,0], expected[:,1], expected[:,2], 'g-')
+ax.plot(actual[:,0], actual[:,1], actual[:,2], 'r--')
+for i in range(len(actual)):
+  xs = [actual[i,0], expected[i,0]]
+  ys = [actual[i,1], expected[i,1]]
+  zs = [actual[i,2], expected[i,2]]
+  ax.plot(xs, ys, zs, 'b-', lw=0.1)
 plt.show()
 
 
